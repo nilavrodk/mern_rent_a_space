4a24f69e49f97c152ddd05f1b35b489bebfd9527
nilavrodk <nilavrodaskabya1@gmail.com>
2023-12-26 21:13:56 +0600

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("conected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("listening");
